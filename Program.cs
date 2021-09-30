8cc8f70e055bd235cb2d1ca3349518fa189e66c4
Jura <nitron1004@mail.ru>
2021-09-30 17:51:58 +0300

    lession 2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter your name");
+
+            var userName = Console.ReadLine();           
+
+            Console.WriteLine($"Hello {userName}! today is: ");
+            Console.WriteLine(DateTime.Now);
+
+
+
+
+        }
+    }
+}
