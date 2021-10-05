b5a8a284ba7fe6588926432681a2ed4ad61a0fdc
Jura <nitron1004@mail.ru>
2021-10-05 15:55:24 +0300

    Lession3

---
+++ Lession33.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lession33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str;
+
+            Console.WriteLine("Enter words: ");
+            str = Console.ReadLine();
+
+            if (str != "")
+            {
+                for (int i = str.Length; i > 0; i--)
+                {
+                    Console.Write(str[i - 1]);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+           
