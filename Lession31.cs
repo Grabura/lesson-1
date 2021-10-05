23fe7924e8daaae85d3dd1a1bdb861a52461956a
Jura <nitron1004@mail.ru>
2021-10-05 15:48:27 +0300

    Lession4

---
+++ Lession31.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lession31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5];
+            Random ran = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int a = 0; a < array.GetLength(1); a++)
+                {
+                    array[i, a] = ran.Next(1, 9);
+                    Console.WriteLine(/*Array elements diagonally*/);
+                }
+            }
+
+            Console.ReadKey();
+                
+
+        }
+    }
+}
---
+++ Lession32.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lession32
+{
+    using System;
+
+    namespace PhoneList
+    {
+        class Program
+        {                
+                 
+            private static int n = 4;
+            private static int m = 5;
+            private static string[,] phoneList = new string[n, m];
+
+            static void Main()
+            { 
+                    Console.Write("Enter name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter phone number and e-mail: ");
+                    string number = Console.ReadLine();
+
+                    for (int i = 0; i < n; i++)
+                        for (int j = 0; j < m; j++)
+                            if (phoneList[i, j] == null)
+                            {
+                                phoneList[i, j] = string.Format("{0} {1}", name, number);
+                                Console.WriteLine("Записанно!");
+                                Console.WriteLine();
+                                return;
+                            }
+            }
+        }
+    }
+}
+
+
+    
